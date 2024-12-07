7def468e2d45b46c58ff1ebcfd744ee5df419361
katushenca <shutenko.katya@bk.ru>
2024-12-07 23:23:13 +0500

    fix line drawing

--- windows/canvas.py
+++ windows/canvas.py
@@ -11,6 +11,7 @@ class Canvas(QGraphicsView):
         self.canvas_entity = canvas_entity
         self.scene = QGraphicsScene(self)
         self.setScene(self.scene)
+        self.previous_pos = None
 
         self.setRenderHint(QPainter.RenderHint.Antialiasing)
         self.setFixedSize(QSize(canvas_entity.width, canvas_entity.height))
@@ -37,19 +38,39 @@ class Canvas(QGraphicsView):
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
             self.is_drawing = True
-            self.DrawPixels(event)
+            self.previous_pos = self.mapToScene(event.pos())
+            self.DrawPixels(self.previous_pos, self.previous_pos)
 
     def mouseMoveEvent(self, event):
         if self.is_drawing:
-            self.DrawPixels(event)
+            current_pos = self.mapToScene(event.pos())
+            self.DrawPixels(self.previous_pos, current_pos)
+            self.previous_pos = current_pos
 
-    def DrawPixels(self, event):
-        self.start_pos = self.mapToScene(event.pos())
-        x, y = self.start_pos.x(), self.start_pos.y()
-        pixel_size = 1
-        self.scene.addRect(QRectF(x, y, pixel_size, pixel_size),
-                           pen=QPen(Qt.PenStyle.NoPen),
-                           brush=QBrush(self.user.Brush.color))
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.is_drawing = False
+            self.previous_pos = None
+
+    def DrawPixels(self, start_pos, end_pos):
+        x1, y1 = start_pos.x(), start_pos.y()
+        x2, y2 = end_pos.x(), end_pos.y()
+
+        steps = int(max(abs(x2 - x1), abs(y2 - y1)))
+        if steps == 0:
+            self.scene.addRect(QRectF(x1, y1, 1, 1),
+                               pen=QPen(Qt.PenStyle.NoPen),
+                               brush=QBrush(self.user.Brush.color))
+            return
+
+        dx = (x2 - x1) / steps
+        dy = (y2 - y1) / steps
+        for i in range(steps + 1):
+            px = x1 + i * dx
+            py = y1 + i * dy
+            self.scene.addRect(QRectF(px, py, 1, 1),
+                               pen=QPen(Qt.PenStyle.NoPen),
+                               brush=QBrush(self.user.Brush.color))
 
     def wheelEvent(self, event):
         event.ignore()
